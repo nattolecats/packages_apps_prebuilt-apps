740831c0c245c7a9ea0d4a948709316835a66ad3
lahaina <nattolecats@gmail.com>
2023-04-09 00:46:20 +0900

    prebuilt-apps: Import LMC 8.4 prebuilt (aweme variant) * For more better camera quality.

--- prebuilt-apps.mk
+++ prebuilt-apps.mk
@@ -6,8 +6,14 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     OPFiles \
     OPGallery \
+    LMC \
     ViperFX
 
 # Permissions
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/OnePlusFileManager/privapp-permissions-op-files.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-op-files.xml
+
+# LMC Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/LMC/LMC8.4_OPN2_Ьy_koЬa_v9.6.xml:LMC8.4/LMC8.4_OPN2_Ьy_koЬa_v9.6.xml \
+    $(LOCAL_PATH)/LMC/De-eM.so:LMC8.4/libs/De-eM.so
