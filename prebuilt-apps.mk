f2503c42dc22fecbc4f8a9428147aa73061c5ebc
lahaina <nattolecats@gmail.com>
2023-05-29 20:57:54 +0900

    prebuilt-apps: Remove xml & libs * Moved into release tab.

--- prebuilt-apps.mk
+++ prebuilt-apps.mk
@@ -6,8 +6,3 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     OPGallery \
     LMC
-
-# LMC Configs
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/LMC/LMC8.4_OPN2_Ьy_koЬa_v9.6.xml:LMC8.4/LMC8.4_OPN2_Ьy_koЬa_v9.6.xml \
-    $(LOCAL_PATH)/LMC/De-eM.so:LMC8.4/libs/De-eM.so
